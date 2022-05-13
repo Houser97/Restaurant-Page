fe0f0834f4f4cb6f9914d0240c32cf4de6f95788
Arturo Rivera <arturo.riverar97@gmail.com>
2022-05-13 15:42:22 -0500

    Create function addHome to fill page
    When one of the options is selected the page is erased before adding
    elements to the div element in the HTML file. At the moment the only
    section created is the home, which is already added by means of the
    function addHome().

--- src/index.js
+++ src/index.js
@@ -3,15 +3,48 @@ let navbar = document.querySelector('.navbar');
 let options = document.querySelectorAll('.option');
 
 function currentOption(e) {
+    let mainContent = document.getElementById('content');
     options.forEach(element => element.classList.remove('activeOption'));
     e.target.classList.add('activeOption');
-    console.log(e.target.textContent)
-    clearPage();
+    clearPage(mainContent);
+
+    if(e.target.textContent == 'Home') addHome(mainContent);
 }
 
-function clearPage() {
-    let mainContent = document.getElementById('content');
+function clearPage(mainContent) {
     mainContent.innerHTML = '';
+}
+
+function addHome(mainContent){
+    let section = document.createElement('section');
+        section.classList.add('home');
+        section.id = 'home';
+    let divContent = document.createElement('div');
+        divContent.classList.add('content')
+    let h3 = document.createElement('h3');
+        h3.textContent = 'Food made with love';
+    let a = document.createElement('a'); 
+        a.classList.add('btn');
+        a.textContent = 'Order Now'
+    let p = document.createElement('p');
+        p.textContent = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui quos tempora eligendi adipisci at fugiat iste accusantium architecto dolore sit voluptatibus tenetur repellat, sunt mollitia, expedita numquam culpa reiciendis quas.'
+
+    let divImage = document.createElement('div'); 
+        divImage.classList.add('image');
+    let img = document.createElement('img');
+        img.src = 'images/home-img.png';
+        
+    divContent.appendChild(h3);
+    divContent.appendChild(p);
+    divContent.appendChild(a);
+
+    divImage.appendChild(img);
+
+
+
+    section.appendChild(divContent);
+    section.appendChild(divImage);
+    mainContent.appendChild(section);
 }
 
 menu.onclick = () => {

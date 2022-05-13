3a8c599c8c8f196bfd434815d79dc7be850fabdc
Arturo Rivera <arturo.riverar97@gmail.com>
2022-05-13 09:56:25 -0500

    Finish header section
    The file CSS leverages media queries to make the header's layout
    responsive. On the other hand, in the index.js new code was added
    to give functionality to the button that must show the user
    the options to go to the menu, contact and home once the screen
    has a specific width.

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Houmser weno");
+(()=>{let e=document.querySelector("#menu-bar"),o=document.querySelector(".navbar");e.onclick=()=>{e.classList.toggle("fa-times"),o.classList.toggle("active")},window.onscroll=()=>{e.classList.remove("fa-times"),o.classList.remove("active")},console.log("Houmser weno")})();

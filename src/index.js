0d5b69b9f3127c2bff7556ccd7f382ef70c8cfa2
Arturo Rivera <arturo.riverar97@gmail.com>
2022-05-13 15:16:30 -0500

    Add funcionality to options Home, Menu and Contact
    In the index.js add events were added to these options to trigger
    the function that adds a blue rectangle to the current option, which
    also calls the function cleanPage() to prepare the page to add the
    corresponding section.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
+let options = document.querySelectorAll('.option');
+
+function currentOption(e) {
+    options.forEach(element => element.classList.remove('activeOption'));
+    e.target.classList.add('activeOption');
+    console.log(e.target.textContent)
+    clearPage();
+}
+
+function clearPage() {
+    let mainContent = document.getElementById('content');
+    mainContent.innerHTML = '';
+}
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
@@ -10,5 +23,7 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+options.forEach(element => element.addEventListener('click', currentOption))
 
 console.log('Houmser weno');
